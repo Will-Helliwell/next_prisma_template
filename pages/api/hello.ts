c8eaa620135fd6f00889c530227d98cc1900a65a
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2023-09-27 08:16:57 +0800

    Change all single to double quotes

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next"
 
 type Data = {
   name: string
@@ -12,5 +12,5 @@ export default function handler(
 
   var test = 1
 
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" })
 }
