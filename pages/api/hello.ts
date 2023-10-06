e212d156ee7952465883a043ce2b23f1da7a3795
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2023-10-06 10:35:12 +0800

    Deliberately fail eslint and prettier

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
+  nameTest: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const test_variable = 1;
+
+
+
+
+
+
+
+
+  res.status(200).json({ nameTest: "John Doe" });
 }
