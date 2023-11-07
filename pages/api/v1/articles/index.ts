fbeea3bea1f8c937fc7233b7de97290cd1c5b8a4
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2023-11-07 10:22:09 +0800

    In development environments, open a single prisma client, store it globally and reuse it

--- pages/api/v1/articles/index.ts
+++ pages/api/v1/articles/index.ts
@@ -1,12 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../../../../services/db";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method === "GET") {
-    const prisma = new PrismaClient();
     try {
       const articles = await prisma.article.findMany();
       res.status(200).json(articles);
@@ -14,8 +13,6 @@ export default async function handler(
       res
         .status(500)
         .json({ error: "An error occurred while fetching articles." });
-    } finally {
-      await prisma.$disconnect();
     }
   } else if (req.method === "POST") {
     res.status(200).json("create a new article");
