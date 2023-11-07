1ea7fb41115f66945ab724187b0d8bbeb947d0c3
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2023-11-07 11:52:24 +0800

    Endpoint best practices - handler functions; case statement, return json object; prisma in try-catch

--- pages/api/v1/articles/index.ts
+++ pages/api/v1/articles/index.ts
@@ -5,18 +5,38 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method === "GET") {
-    try {
-      const articles = await prisma.article.findMany();
-      res.status(200).json(articles);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "An error occurred while fetching articles." });
-    }
-  } else if (req.method === "POST") {
-    res.status(200).json("create a new article");
-  } else {
-    res.status(405).end();
+  switch (req.method) {
+    case "GET":
+      await handleGetRequest(req, res);
+      break;
+
+    case "POST":
+      await handlePostRequest(req, res);
+      break;
+
+    default:
+      res.status(405).json({ message: "InvalidMethod" });
+      break;
   }
+}
+
+async function handleGetRequest(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> {
+  try {
+    const articles = await prisma.article.findMany();
+    res.status(200).json({ articles });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching articles." });
+  }
+}
+
+async function handlePostRequest(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> {
+  res.status(200).json("create a new article");
 }
