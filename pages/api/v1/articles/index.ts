b8821b8fb7a9887162aeba17632cff0acd6c2c3d
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2023-11-08 09:22:38 +0800

    Install zod. Use to validate get request query params. Get articles now has optional limit

--- pages/api/v1/articles/index.ts
+++ pages/api/v1/articles/index.ts
@@ -1,9 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@project-root/services/db";
+import { z } from "zod";
 import {
   getErrorHttpStatusCode,
   getErrorMessage,
 } from "@project-root/utils/errors";
+
+const getQueryParamsSchema = z.object({
+  limit: z
+    .string()
+    .refine((str) => !isNaN(+str), {
+      message: "Limit must be a valid number.",
+    })
+    .optional(),
+});
 
 export default async function handler(
   req: NextApiRequest,
@@ -29,7 +39,17 @@ async function handleGetRequest(
   res: NextApiResponse,
 ): Promise<void> {
   try {
-    const articles = await prisma.article.findMany();
+    let articles = {};
+    const getQueryParams = getQueryParamsSchema.parse(req.query);
+    const { limit } = getQueryParams;
+    if (limit === undefined) {
+      articles = await prisma.article.findMany();
+    } else {
+      const limitAsNumber = parseInt(limit);
+      articles = await prisma.article.findMany({
+        take: limitAsNumber,
+      });
+    }
     res.status(200).json({ articles });
   } catch (error) {
     const httpStatusCode = getErrorHttpStatusCode(error);
